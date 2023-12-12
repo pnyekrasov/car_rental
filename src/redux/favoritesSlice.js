066c92bc898f671711aa2bdcfc2d323323d857ac
pnyekrasov <pnekrasov1910@gmail.com>
2023-12-12 18:00:49 +0200

    Update favoritesSlice.js

--- src/redux/favoritesSlice.js
+++ src/redux/favoritesSlice.js
@@ -14,15 +14,16 @@ export const { getFavoritesCar } = favoritesSlice.actions;
 export const filterReducer = favoritesSlice.reducer;
 
 
-reducers: {
-  addOrRemoveToFavorites: (state, action) => {
-    const carToAdd = action.payload;
+// reducers: {
+//   addOrRemoveToFavorites: (state, action) => {
+//     const carToAdd = action.payload;
 
-    if (!state.list.some((car) => car.id === carToAdd.id)) {
-      state.list.push(carToAdd);
-    } else {
-      const carToDelete = action.payload;
-      state.list = state.list.filter((car) => car.id !== carToDelete.id);
-    }
-  },
-},
+//     if (!state.list.some((car) => car.id === carToAdd.id)) {
+//       state.list.push(carToAdd);
+//     } else {
+//       const carToDelete = action.payload;
+//       state.list = state.list.filter((car) => car.id !== carToDelete.id);
+//     }
+//   },
+// }
+// }
